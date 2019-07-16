4bf83a39a3a55edf512aeb47d8feacce73dfe859
Navaneeth Dev <lezendary73@gmail.com>
2019-07-16 17:10:47 +0530

    Made script more user friendly

--- script.py
+++ script.py
@@ -1,28 +1,35 @@
 """
 Command-line Workbook - by Lezend
 """
-import json
-from time import sleep
+import json, sys, time
 from difflib import get_close_matches
 
-data = json.load(open("data.json"))
+def print_slow(output):
+    for letter in output:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.02)
 
 def out(w):
-    print("%s:" %w.capitalize())
+    print_slow(w.capitalize()+"\n")
     for i in range(len(data[w])):
-        print("%d.%s" %(i+1,data[w][i]))
+        print_slow("%d. %s\n" %(i+1,data[w][i]))
 
 def suggest(matches):
     if len(matches) > 0:
-        yn=input("Did you mean %s instead?(Y/N): "%matches[0].capitalize())
+        print_slow("Did you mean %s instead?(Y/N): "%matches[0].capitalize())
+        yn=input().upper()
         if yn == "Y":
             out(matches[0])
             return
         elif yn == "N":
             matches.pop(0)
             suggest(matches)
+        else:
+            print_slow("Please enter 'Y' for yes and 'N' for no\n")
+            suggest(matches)
     else:
-        print("Ran out of Options! Please double check your word.")
+        print_slow("Ran out of Options! Please double check your word.\n")
 
 def meaning(w):
     matches=get_close_matches(w,data.keys(),cutoff=0.8)
@@ -31,18 +38,28 @@ def meaning(w):
     elif len(matches) > 0:
         suggest(matches)
     else:
-        print("The word doesn't make sense, please double check it.")
+        print_slow("The word doesn't exist in the English language.")
 
 def read_inp(inp):
     if inp=="Y":
-        meaning(input("Enter a word: ").lower())
-        read_inp(input("\nWould you like to try again?(Y/N): "))
+        print_slow("Enter a word: ")
+        meaning(input().lower())
+        print_slow("\nWould you like to try again?(Y/N): ")
+        read_inp(input().upper())
     elif inp=="N":
-        print("\n\t\t\tTHANK YOU!\n")
-        sleep(1)
+        print_slow("\n\t\t\tT H A N K  Y O U !\n\n")
+        time.sleep(1)
         exit()
     else:
-        read_inp(input("Please enter 'Y' for yes and 'N' for no: "))
+        print_slow("Please enter 'Y' for yes and 'N' for no: ")
+        read_inp(input().upper())
 
-print("\n\n\t\tCOMMAND-LINE WORDBOOK - by Lezend\n")
+data = json.load(open("data.json"))
+
+print("\n\n\t\t", end='')
+for letter in "COMMAND LINE WORDBOOK\n\t\t\t BY LEZEND":
+    print(letter + " ", end='')
+print("\n")
+time.sleep(1)
+
 read_inp("Y")
